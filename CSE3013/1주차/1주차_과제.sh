f36f79f8c80e04354e4c39f36d2f3c6498566458
YONGYONGA <103048447+YONGYONGA@users.noreply.github.com>
2023-06-06 15:47:11 +0900

    Add files via upload

---
+++ CSE3013/1주차/1주차_과제.sh
@@ -0,0 +1,33 @@
+﻿#!/bin/bash
+echo 'working directory:'
+read dirname
+if [ -n "$dirname" ] 
+then
+	echo >dummy
+	rm dummy
+else
+	echo 'no input'
+	exit 1
+
+fi
+if [ -d "$dirname" ]
+then
+	cd $dirname
+	if [ $? -eq 0 ]
+	then
+		echo >dummy
+		rm dummy
+	else
+		echo "please 접근권한"
+		exit 1
+	fi
+else
+	echo 'wrong directory'
+	exit 1
+fi	
+for dir in `ls $dirname`
+do
+	chmod 777 $dir 
+	newname=`echo $dir | tr "[a-z] [A-Z]" "[A-Z] [a-z]"`
+	mv $dir $newname  
+done
